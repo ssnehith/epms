301ef88f75d9d8b6e1d16e5d9fbe3fdf81535ac7
Snehith <ssssnehith07@gmail.com>
2025-12-22 11:16:52 +0530

    Initial commit: Spring Boot EPMS setup

---
+++ src/main/java/io/riqueza/EpmsApplication.java
@@ -0,0 +1,13 @@
+package io.riqueza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpmsApplication.class, args);
+	}
+
+}
