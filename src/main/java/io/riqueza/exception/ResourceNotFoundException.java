d4819c35ec0394d8c5930fc0f6e69f7271fb8c59
Snehith <ssssnehith07@gmail.com>
2025-12-23 09:45:19 +0530

    Add update and delete employee APIs

---
+++ src/main/java/io/riqueza/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package io.riqueza.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/io/riqueza/service/EmployeeService.java
+++ src/main/java/io/riqueza/service/EmployeeService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import io.riqueza.dto.CreateEmployeeRequest;
+import io.riqueza.dto.UpdateEmployeeRequest;
 import io.riqueza.entity.Employee;
+import io.riqueza.exception.ResourceNotFoundException;
 import io.riqueza.repository.EmployeeRepository;
 
 @Service
@@ -25,5 +27,24 @@ public class EmployeeService {
     Employee emp = new Employee(req.getName(), req.getEmail(), req.getDepartment(), req.getBaseSalary());
     return employeeRepository.save(emp);
     }
+
+    public Employee updateEmployee(Long id, UpdateEmployeeRequest req) {
+    Employee emp = employeeRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id: " + id));
+
+    emp.setName(req.getName());
+    emp.setEmail(req.getEmail());
+    emp.setDepartment(req.getDepartment());
+    emp.setBaseSalary(req.getBaseSalary());
+
+    return employeeRepository.save(emp);
+    }
+
+    public void deleteEmployee(Long id) {
+        Employee emp = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id: " + id));
+        employeeRepository.delete(emp);
+    }
+
 
 }
